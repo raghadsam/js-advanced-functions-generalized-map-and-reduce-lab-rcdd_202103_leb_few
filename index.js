4dc62d36a79e21a11f5b3499c9a3cf997a0a62e3
Raghad Al Samrout <raghadsamrout@ieee.org>
2021-05-04 00:47:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your functions here
+
+function map(sourceArray,f){
+  let result=[];
+  for(let i=0;i<sourceArray.length;i++)
+  {
+     result.push(f(sourceArray[i]));
+  }
+  return result;
+}
+function reduce(sourceArray,cb,starting){
+  let r=(!!starting) ? starting: sourceArray[0];
+  let i=(!!starting) ? 0 :1;
+
+  for(;i<sourceArray.length;i++){
+    r=cb(sourceArray[i],r)
+  }
+  return r;
+}
